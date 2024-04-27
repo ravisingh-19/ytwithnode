147dc6c875a34ade35e809ccb5a6199da77c11f0
Ravi Singh <ravisinghabes@gmail.com>
2024-04-27 08:35:59 +0530

    added user controller/router

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
             .catch(err => next(err))
     }
@@ -11,7 +11,7 @@ export { asyncHandler };
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
-//         await fn(req, res, next);
+//         return await fn(req, res, next);
 //     } catch (err) {
 //         res.status(err.code || 500).json({
 //             success: false,
